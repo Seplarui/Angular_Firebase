df3f99243335ec7167d166a6fc6d52d5cd71a3c1
Sebas <seplarui@gmail.com>
2018-08-10 23:28:02 +0200

    CRUD

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+import { Product } from '../models/product';
+@Injectable()
+
 export class ProductService {
 
-  constructor() { }
+  productList: AngularFireList<any>;
+
+  constructor(private firebase: AngularFireDatabase) { }
+
+  getProduct() {
+    this.productList = this.firebase.list('products');
+  }
+  insertProduct(product: Product) {
+
+    this.productList.push({
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+  updateProduct(product: Product) {
+    this.productList.update(product.$key, {
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+
+  deleteProduct($key: string) {
+    this.productList.remove($key);
+  }
 }
